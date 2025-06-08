c56a80899ef923798b6dcdb444b3ce5bb88d6538
snw1317 <46914962+snw1317@users.noreply.github.com>
2025-06-08 14:39:52 -0500

    Remove unused helpers from serialization

--- noether_gui/src/widgets/tpp_widget.cpp
+++ noether_gui/src/widgets/tpp_widget.cpp
@@ -12,6 +12,7 @@
 #include <QStandardPaths>
 #include <QTextStream>
 #include <yaml-cpp/yaml.h>
+#include <noether_tpp/core/serialization.h>
 
 // Rendering includes
 #ifndef VTK_MAJOR_VERSION
@@ -563,53 +564,18 @@ void TPPWidget::onSaveToolPath(const bool /*checked*/)
   if (!file.endsWith(".yaml"))
     file = file.append(".yaml");
 
-    try
+  try
   {
-    // Open output file
     std::ofstream out(file.toStdString());
     if (!out)
       throw std::runtime_error("Failed to open file for writing: " + file.toStdString());
 
-    // Write waypoints one at a time
-    for (const auto& fragment : tool_paths_)
-    {
-      for (const auto& path : fragment)
-      {
-        for (const auto& segment : path)
-        {
-          for (const auto& pose : segment)
-          {
-            // Write header
-
-            out << "---\n"; 
-            out << "header:\n";
-            out << "  stamp:\n";
-            out << "    sec: 0\n";
-            out << "    nanosec: 0\n";
-            out << "  frame_id: world\n";
-
-            // Write pose
-            out << "pose:\n";
-            out << "  position:\n";
-            out << "    x: " << pose.translation().x() << "\n";
-            out << "    y: " << pose.translation().y() << "\n";
-            out << "    z: " << pose.translation().z() << "\n";
-
-            // Convert rotation matrix to quaternion and write orientation
-            Eigen::Quaterniond q(pose.rotation());
-            out << "  orientation:\n";
-            out << "    x: " << q.x() << "\n";
-            out << "    y: " << q.y() << "\n";
-            out << "    z: " << q.z() << "\n";
-            out << "    w: " << q.w() << "\n";
-          }
-        }
-      }
-    }
-      }
+    YAML::Node node(tool_paths_);
+    out << node;
+  }
   catch (const std::exception& ex)
   {
-        std::stringstream ss;
+    std::stringstream ss;
     printException(ex, ss);
     QMessageBox::warning(this, "Save Error", QString::fromStdString(ss.str()));
   }
